74abe88c688c5c9e5c542aab59be9d065a59ee8b
atharva-2001 <aryaa_1@rknec.edu>
2021-01-04 16:19:32 +0530

    plotted sphere with labels, but too many traces is a problem

--- tests/test.py
+++ tests/test.py
@@ -1,53 +1,98 @@
-import wikipediaapi
-wiki_wiki = wikipediaapi.Wikipedia('en')
-page_py = wiki_wiki.page('Atom')
+import plotly.express as px
+import plotly.graph_objects as go
+import math
+import numpy as np
+import requests 
 
-def print_sections(sections, level=0):
-        for s in sections:
-                print("%s: %s - %s" % ("*" * (level + 1), s.title, s.text[0:40]))
-                print_sections(s.sections, level + 1)
+def art_from_origin(): 
+    S = requests.Session()
+
+    URL = "https://en.wikipedia.org/w/api.php"
+
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "titles": "Atom",
+        "prop": "links",
+        "pllimit": "max"
+    }
+
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+    # print(DATA)
+    PAGES = DATA["query"]["pages"]
+
+    art = []
+    for k, v in PAGES.items():
+        for l in v["links"]:
+            art.append(l["title"])
+    return art
+
+def create_random_populated_sphere(radius, points, plot_flag, show_lines_with_origin):
+    '''
+    this function will return the figure and the coordinates in a tuple
+    when asked to plot, it will plot it. 
+    enter radius ... integer, radius of the sphere
+    points...list having the markers
+    plot_flag...boolean, whether or not to plot the sphere
+    show_lines_with_origin...boolean, whether to show connections or not
+    returns (coordinates of the sphere and the fig in a tuple)
+    '''
+    coor = [[],[],[]]
+    index = 0
+    while True:
+        if index > len(points)-1:
+            break
+        
+        x = (-1)**np.random.randint(5, size = 1)[0] * radius * np.random.rand(1)[0]
+        y = (-1)**np.random.randint(5, size = 1)[0] * radius * np.random.rand(1)[0]
+        z = (-1)**np.random.randint(5, size = 1)[0] * radius * np.random.rand(1)[0]
+  
+        if math.sqrt(x**2 + y**2 + z**2) <= radius: 
+            coor[0].append(x)
+            coor[1].append(y)
+            coor[2].append(z)
+            index+=1
+
+    print(len(coor[0]), len(points))
+    fig = go.Figure()
+    fig.add_trace(go.Scatter3d(
+        x = coor[0],
+        y = coor[1],
+        z = coor[2],
+        text = points,
+        marker=dict(
+            size=5,
+            colorscale='Viridis',   # choose a colorscale
+            opacity=0.8
+        ),
+        mode = "markers"
+    ))
 
 
-# print_sections(page_py.sections)
-link_data, link_title = [],[]
+    for x_coor,y_coor,z_coor in zip(coor[0], coor[1], coor[2]): 
+        fig.add_trace(go.Scatter3d(
+        x = [x_coor,0],
+        y = [y_coor,0],
+        z = [z_coor,0],
+        text = points,
+        marker=dict(
+            size=0.1,
+            colorscale='Viridis',   # choose a colorscale
+            opacity=0.8
+        ),
+        mode = "lines"))
 
-links = page_py.links
-for link in links.keys():
-    link_title.append(link)
-    link_data.append(links[link])
-
-# for a, b in zip(link_data, link_title):
-#     print(a,"==>>",b)
-
-# wiki_html = wikipediaapi.Wikipedia(
-#         language='en',
-#         extract_format=wikipediaapi.ExtractFormat.HTML
-# )
-# p_html = wiki_html.page("Atom")
-# print(p_html.text)
+    fig.update_layout(
+        height  =1300, width = 2000,
+    )
+    fig.update_traces(showlegend = False)   
+    if plot_flag == True:
+        fig.show()
 
 
-import requests
+    return (coor, fig)
 
-S = requests.Session()
+art = art_from_origin()
+_, _ = create_random_populated_sphere(radius=1000, points=art, plot_flag=True, show_lines_with_origin=True)
 
-URL = "https://en.wikipedia.org/w/api.php"
-
-PARAMS = {
-    "action": "query",
-    "format": "json",
-    "titles": "Atom",
-    "prop": "links",
-    "pllimit": "max"
-}
-
-R = S.get(url=URL, params=PARAMS)
-DATA = R.json()
-
-PAGES = DATA["query"]["pages"]
-
-for k, v in PAGES.items():
-    for l in v["links"]:
-        print(l["title"])
-
-print(DATA)
