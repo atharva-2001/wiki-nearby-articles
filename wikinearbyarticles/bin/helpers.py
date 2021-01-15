863ab64aa4221631c4f91fc6808e6c82968493e0
atharva-2001 <aryaatharva18@gmail.com>
2021-01-15 17:59:27 +0530

    tests

--- wikinearbyarticles/bin/helpers.py
+++ wikinearbyarticles/bin/helpers.py
@@ -8,18 +8,15 @@ from time import sleep
 import sys
 import os
 
+
 def find_hover_text(str):
     lst = re.split(" ", str)
-    lst = [" ".join(lst[i : i + 3]) for i in range(0, len(lst), 3)]
+    lst = [" ".join(lst[i: i + 3]) for i in range(0, len(lst), 3)]
     str = "<br>".join(["".join(item) for item in lst])
     return str
 
-# 
-#  @todo hover should call api that returns summary data 
-#  @body helpers class doesnt automatically call data, use the class to call hover data for other article names too
-# 
-# 
-def random_points_in_a_sphere(h = 0, g = 0, f = 0, num = 0, radius = 5):
+
+def random_points_in_a_sphere(h=0, g=0, f=0, num=0, radius=5):
     coor = [[], [], []]
     index = 0
     while True:
@@ -30,7 +27,7 @@ def random_points_in_a_sphere(h = 0, g = 0, f = 0, num = 0, radius = 5):
         y = (-1) ** np.random.randint(5, size=1)[0] * radius * np.random.rand(1)[0]
         z = (-1) ** np.random.randint(5, size=1)[0] * radius * np.random.rand(1)[0]
 
-        if math.sqrt((x-h) ** 2 + (y-g) ** 2 + (z-f) ** 2) <= radius: # checking if the point is in the circle
+        if math.sqrt((x - h) ** 2 + (y - g) ** 2 + (z - f) ** 2) <= radius:  # checking if the point is in the circle
             coor[0].append(x)
             coor[1].append(y)
             coor[2].append(z)
@@ -39,41 +36,44 @@ def random_points_in_a_sphere(h = 0, g = 0, f = 0, num = 0, radius = 5):
 
 
 def spinning_cursor():
-  while True:
-    for cursor in '\\|/-':
-      sleep(0.1)
-      # Use '\r' to move cursor back to line beginning
-      # Or use '\b' to erase the last character
-      sys.stdout.write('\r{}'.format(cursor))
-      # Force Python to write data into terminal.
-      sys.stdout.flush()
+    while True:
+        for cursor in '\\|/-':
+            sleep(0.1)
+            # Use '\r' to move cursor back to line beginning
+            # Or use '\b' to erase the last character
+            sys.stdout.write('\r{}'.format(cursor))
+            # Force Python to write data into terminal.
+            sys.stdout.flush()
+
 
 def get_calls(article_name, number_of_lines=7):
     S = requests.Session()
     URL = "https://en.wikipedia.org/w/api.php"
 
     PARAMS = {
-                "action": "query",
-                "format": "json",
-                "titles": article_name,
-                "prop": "extracts",
-                "exsentences": number_of_lines,
-                "exlimit": "1",
-                "explaintext": "1",
-                "formatversion": "2",
-                }
+        "action": "query",
+        "format": "json",
+        "titles": article_name,
+        "prop": "extracts",
+        "exsentences": number_of_lines,
+        "exlimit": "1",
+        "explaintext": "1",
+        "formatversion": "2",
+    }
     R = S.get(url=URL, params=PARAMS)
     DATA = R.json()
     sys.stdout.flush()
     # print(DATA)
     # os._exit(0)
     return DATA
+
+
 class wna:
     def __init__(
-        self,
-        link,
-        prop_params,
-        points_in_one_plot=15,
+            self,
+            link,
+            prop_params,
+            points_in_one_plot=15,
     ):
         self.link = link
         if "wiki" not in link:
@@ -109,7 +109,7 @@ class wna:
                 points.append(l["title"])
 
         points = [
-            points[i : i + self.points_in_one_plot]
+            points[i: i + self.points_in_one_plot]
             for i in range(0, len(points), self.points_in_one_plot)
         ]
 
@@ -119,16 +119,17 @@ class wna:
         self.points = points
         print("got points")
 
-    def return_points(self, plot_index = 0):
-        if self.points == []:
+    def return_points(self, plot_index=0):
+        if not self.points:
             self.collect_points()
         return self.points[plot_index]
 
-    def article_summary_for_hover(self, number_of_lines=2, plot_index=0, display_all_summaries = False, collect_points = True):
-        if collect_points == True: # TODO fix this
+    def article_summary_for_hover(self, number_of_lines=2, plot_index=0, display_all_summaries=False,
+                                  collect_points=True):
+        if collect_points:  # TODO fix this
             self.collect_points()
             self.points = self.points[plot_index]
-        if display_all_summaries == True:
+        if display_all_summaries:
             S = requests.Session()
             URL = "https://en.wikipedia.org/w/api.php"
 
@@ -162,27 +163,26 @@ class wna:
         else:
             print(f"trying to get hover text for {self.article_name}")
             return get_calls(article_name=self.article_name)
-            
 
     def plot_points(
-        self,
-        dis_to_external_point = 10,
-        external_points = [],
-        center = "",
-        radius=5,
-        plot_flag=False,
-        show_lines_with_origin=True,
-        line_color="#5c5c5c",
-        dot_color="#525BCB",
-        plot_index=0,
-        number_of_lines=2,
+            self,
+            dis_to_external_point=10,
+            external_points=[],
+            center="",
+            radius=5,
+            plot_flag=False,
+            show_lines_with_origin=True,
+            line_color="#5c5c5c",
+            dot_color="#525BCB",
+            plot_index=0,
+            number_of_lines=2,
     ):
         # collect summary data and plot points
         self.article_summary_for_hover(
             plot_index=plot_index, number_of_lines=number_of_lines
         )
 
-        coor = random_points_in_a_sphere(num = len(self.points), radius = 5)
+        coor = random_points_in_a_sphere(num=len(self.points), radius=5)
 
         # print(len(coor[0]), len(points))
         fig = go.Figure()
@@ -200,7 +200,7 @@ class wna:
         )
 
         for x_coor, y_coor, z_coor, point in zip(
-            coor[0], coor[1], coor[2], self.points
+                coor[0], coor[1], coor[2], self.points
         ):
             fig.add_trace(
                 go.Scatter3d(
@@ -244,9 +244,8 @@ class wna:
             }
         )
         fig.update_traces(showlegend=False)
-        if plot_flag == True:
+        if plot_flag:
             fig.show()
 
         # self.fig = fig
         return fig
-
